48089d451b0fd2484b886f20f70ec2e59a17dce4
quesadaao@hotmail.com <quesadaao@hotmail.com>
2022-03-09 13:23:29 -0600

    adding MappingProfile

---
+++ BackEnd/BackEnd/API/Mapping/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using data = DAL.DO.Objects;
+
+namespace API.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //CreateMap<data.Foci, DataModels.Foci>().ReverseMap();
+            //CreateMap<data.GroupInvitations, DataModels.GroupInvitations>().ReverseMap();
+            //CreateMap<data.GroupRequests, DataModels.GroupRequests>().ReverseMap();
+            //CreateMap<data.Groups, DataModels.Groups>().ReverseMap();
+            //CreateMap<data.GroupUpdates, DataModels.GroupUpdates>().ReverseMap();
+        }
+    }
+}

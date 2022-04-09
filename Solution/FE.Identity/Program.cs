9e19a8d3c97d8945946d3f961b00c33c1a6e754a
Oscar.Quesada <quesadaao@hotmail.com>
2022-04-08 20:38:45 -0600

    Codigo realizado de FE + Identity + Controllers y Views de Categories + Products

---
+++ Solution/FE.Identity/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FE.Identity
+{
+    public class Program
+    {
+        // URL Del API 
+        public static string baseurl = "http://localhost:44143";
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
